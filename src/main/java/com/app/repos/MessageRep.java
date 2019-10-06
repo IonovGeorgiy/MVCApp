159c4d2758b4f2ad7545102fb2482684a0f8a079
Georgiy Ionov <gigaionov1@gmail.com>
2019-10-06 17:52:52 +0300

    Work with Spring Security and Freemarker

--- src/main/java/com/app/repos/MessageRep.java
+++
@@ -1,7 +0,0 @@
-package com.app.repos;
-
-import com.app.domain.Message;
-import org.springframework.data.repository.CrudRepository;
-
-public interface MessageRep extends CrudRepository <Message, Integer> {
-}
